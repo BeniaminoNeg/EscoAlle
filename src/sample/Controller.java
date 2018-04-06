560653ebeed54b4df3a0b7c3cc66cc0c873153e9
Beniamino Negrini <beniamino.negrini@gmail.com>
2018-04-06 16:35:37 +0200

    primo commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.scene.input.MouseEvent;
+
+import java.time.Duration;
+
+public class Controller {
+
+    private static final Duration DURATA_TURNO = Duration.of()
+    public void computeExit(MouseEvent mouseEvent) {
+    }
+}
